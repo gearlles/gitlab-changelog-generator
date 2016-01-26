679d77d3bc9e9ed0132bd9b8acf83b34174e343d
Gearlles Ferreira <gearlles@gmail.com>
2016-01-26 00:43:24 -0300

    Add project validation

--- gitlab_client.rb
+++ gitlab_client.rb
@@ -7,14 +7,20 @@ class GitlabClient
     Gitlab.endpoint = endpoint
     Gitlab.private_token = private_token
     @project_name = project_name
-    projects = Gitlab.project_search(project_name)
-    if projects.count == 0
-      raise ArgumentError, 'Project not found.'
+    projects = nil
+    begin
+      projects = Gitlab.project_search(project_name)
+      if projects.nil? or projects.count == 0
+        raise ArgumentError, 'Project not found.'
+      else
+        @project = projects[0]
+      end
+    rescue => ex
+      puts ex.message
     end
-    @project = projects[0]
   end
 
   def generate_changelog()
-    
+
   end
 end
