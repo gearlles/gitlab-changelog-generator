c2ff16b71fded7ad4996dd6bd8caebe92eaa4087
Gearlles Ferreira <gearlles@gmail.com>
2016-01-26 00:14:02 -0300

    Add Gitlab gem

--- main.rb
+++ main.rb
@@ -1,15 +1,20 @@
 require 'slop'
+require 'gitlab'
 
 opts = Slop.parse do |o|
   o.string '-u', '--url', 'Project API URL'
-  o.integer '-t', '--token', 'Private token'
+  o.string '-p', '--project', 'Project name'
+  o.string '-t', '--token', 'Private token'
   o.on '--help', 'Print the help' do
     puts o
     exit
   end
 end
 
-projects = Gitlab.project_search('gitlab-changelog-generator')
+Gitlab.endpoint = opts[:url]
+Gitlab.private_token = opts[:token]
+
+projects = Gitlab.project_search(opts[:project])
 if projects.count == 0
   raise ArgumentError, 'Project not found.'
 end
